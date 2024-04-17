c3602977c30c42171324f431db84fa9aacbef0a2
aaditverma <aaditverma@gmail.com>
2024-04-17 12:38:16 +0530

    about and contact

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,6 @@ const App = () =>  {
         <div className="bg-hero-pattern bg-cover bg-no-repeat bg-center">
           <Navbar />
           <Hero />
-
         </div>
         <About />
         <Experience />
